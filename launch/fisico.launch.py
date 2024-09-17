c91f53ac4c03d0c93cc490afebc7843705e8df9e
Santiago-RZ <rioss9463@gmail.com>
2024-09-17 15:06:29 -0600

    fisico_launch

--- launch/fisico.launch.py
+++ launch/fisico.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
             output="screen",
             remappings=[
                 ("__ns","/pi_camera")
-            ]
+            ],
             parameters=[{
                 "video_device":"/dev/video2"
             }]
